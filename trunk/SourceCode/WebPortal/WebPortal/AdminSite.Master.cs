2a26a0f02cdf68a0d31bebb0ace7cc3eaaac7d0d
thuyduong1017@gmail.com <thuyduong1017@gmail.com@6ba9de89-57de-f541-5f0f-ca5a81126c2b>
2011-09-09 17:43:02 +0000

    Phân quyền truy cập trang

--- trunk/SourceCode/WebPortal/WebPortal/AdminSite.Master.cs
+++ trunk/SourceCode/WebPortal/WebPortal/AdminSite.Master.cs
@@ -9,9 +9,30 @@ namespace WebPortal
 {
     public partial class AdminSite : System.Web.UI.MasterPage
     {
+        WebPortal.Repository.Application appRepository = new WebPortal.Repository.Application();
+        WebPortal.Repository.User userRepository = new WebPortal.Repository.User();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public bool CheckDecentralization(string filePath)
+        {
+            string filepath = Request.Path.Substring(1, Request.Path.Length - 1);
+            if (filepath != "NoAccess.aspx")
+            {
+                int userID = userRepository.GetUserIDByUsername(Libs.LibSession.Get(Libs.Constants.ACCOUNT_LOGIN).ToString());
+                List<WebPortal.Model.Application> AppList = appRepository.GetAllAppByUser(userID);
+                var app = appRepository.GetApplicationByFilePath(filepath);
+                foreach (var ap in AppList)
+                {
+                    if (ap.ApplicationID == app.ApplicationID)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
---
+++ trunk/SourceCode/WebPortal/WebPortal/NoAccess.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminSite.Master" AutoEventWireup="true"
+    CodeBehind="NoAccess.aspx.cs" Inherits="WebPortal.NoAccess" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <div class="content-box">
+        <div class="box-body">
+         <div class="box-wrap clear">
+            <div style="height: 300px; color:Red; font-size:18px;">
+                <p>
+                    <b>Bạn không có quyền truy cập trang này! </b>
+                </p>
+            </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
