a1f6bf350fa1ea880f763e34c07eaf3f59ed5d1e
daothihue12 <daothihue12@6ba9de89-57de-f541-5f0f-ca5a81126c2b>
2011-09-13 02:35:12 +0000

    thêm trang LienHe_GopY.aspx

---
+++ trunk/SourceCode/WebPortal/WebPortal/LienHe_GopY.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+namespace WebPortal
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnGui_Click(object sender, EventArgs e)
+        {
+            string chuoigui;
+            chuoigui = txtNoiDung.Text;
+            SendMail(txtEmail.Text, "Thông Tin Giỏ Hàng", chuoigui);
+            lblStatus.Text = "Góp ý của bạn đã được gởi đi...";
+            Response.Redirect("Default.aspx");
+
+        }
+
+        protected void btnHuy_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("");
+        }
+        //ham gui mail
+        public void SendMail(string to, string subject, string body)
+        {
+            try
+            {
+                System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+                mail.From = new MailAddress("mytrang1088@gmail.com");
+                mail.To.Add(to);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.BodyEncoding = System.Text.Encoding.UTF8;
+                mail.IsBodyHtml = true;
+
+                //gui mail bang server cua mail
+                //khoi tao Smtpclient
+                SmtpClient client = new SmtpClient();
+                client.Host = "smtp.gmail.com";
+                //thuoc tinh credentials dung de dua thong tin dang nhap vao server SMTP
+                client.Credentials = new System.Net.NetworkCredential("mytrang1088@gmail.com", "tinhyeucuatoi");
+                client.EnableSsl = true;
+                client.Send(mail);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
